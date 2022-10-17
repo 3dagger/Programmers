232d4d929120f157c3e5489ea9b0ab2360cdc6ab
3dagger <dlqtn36@gmail.com>
2022-10-18 08:44:58 +0900

    문제 추가

---
+++ 두 수의 곱/ProductOfTwoNumbers/src/main/kotlin/Solution.kt
@@ -0,0 +1,12 @@
+class Solution {
+	fun solution(num1: Int, num2: Int): Int {
+		println(num1 * num2)
+		return num1 * num2
+	}
+}
+
+fun main() {
+	val sl = Solution()
+	sl.solution(3, 4)
+	sl.solution(27, 19)
+}
